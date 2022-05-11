e977cf9c320f84539c8233f07c9f6be82d2d82b0
GUSTAVO RENE DIAS BOAMORTE <gustavo.rdboamorte@senacsp.edu.br>
2022-05-11 16:55:40 -0300

    Segundo Projeto de React

---
+++ segundo/src/Footer.js
@@ -0,0 +1,13 @@
+function Footer (){
+    return(
+        <div className="footer">
+            <h3>Consulte a Programação completa em  
+                <a href="http://www.sp.senac.br/tatuape">
+                    &nbsp;sp.senac.br/tatuape 
+                </a> 
+                &nbsp;Escaneie o QR Code 
+            </h3>
+        </div>
+    );
+}
+export default Footer;
---
+++ segundo/src/Main.js
@@ -0,0 +1,25 @@
+function Main(){
+    return(
+        <div className="main">
+            <h4>Bem-Estar</h4>
+            <ul>
+                <li>Reiki-Master</li>
+                <li>Reiki-Nível 1 e 2</li>
+                <li>Reiki-Nível 3</li>
+            </ul>
+
+            <h4>Tecnologia da Informação</h4>
+            <ul>
+                <li>Autocad - Projetos em 2D</li>
+                <li>Excel 2019</li>
+                <li>Excel 2019 - Avançado</li>
+                <li>Introdução ao Office - Word, Excel e Powerpoint 2019</li>
+                <li>Photoshop - Tratamento de Imagem</li>
+                <li>Sketchup PRO</li>
+                <li>Wordpress I - Criação e Gerenciamento de Sites</li>
+            </ul>
+            <img className="img" alt="image reiki" src="https://images.unsplash.com/photo-1506126613408-eca07ce68773?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=799&q=80"></img>
+        </div>
+    );
+}
+export default Main;
